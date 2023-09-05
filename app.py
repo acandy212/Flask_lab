7992099919119fd4c624285b9ffa7262da70817c
acandy212 <candies_amanda@students.columbusstate.edu>
2023-09-05 18:21:21 +0000

    added template.html

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def hello():                      # method called hello
 def hello_name(name):              # call method hello_name
     return "Hello "+ name          # which returns "hello + name  
 
+@app.route("/about")                
+def about():                    
+    return render_template("about.html")   
+
 if __name__ == "__main__":        # when running python app.py
     app.run()                     # run the flask app
 
